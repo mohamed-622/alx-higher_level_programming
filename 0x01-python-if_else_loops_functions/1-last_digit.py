491421d3fffd4ce32083c3c610392ca3d8a25174
mohamed-622 <mohamedezghoudi@gmail.com>
2023-06-06 11:09:15 -0700

    no need to check the sign

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -2,18 +2,12 @@
 import random
 number = random.randint(-10000, 10000)
 
-# check for negative numbbers
-if number < 0:
-    lastDigit = abs(number) % 10
-else:
-    lastDigit = number % 10
+lastDigit = abs(number) % 10
 
 # check the last digit of the number
 if lastDigit == 0:
-    print(f"Last digit of {number:d} is {lastDigit:d} and is 0")
+    print(f"Last digit of {number} is {lastDigit:d} and is 0")
 elif lastDigit > 5:
-    print(f"Last digit of {number:d} is {lastDigit:d} and is greater\
- than 5")
+    print(f"Last digit of {number} is {lastDigit:d} and is greater than 5")
 elif lastDigit < 6 and lastDigit != 0:
-    print(f"Last digit of {number:d} is {lastDigit:d} and is\
- less than 6 and not 0")
+    print(f"Last digit of {number} is {lastDigit:d} and is less than 6 and not 0")
