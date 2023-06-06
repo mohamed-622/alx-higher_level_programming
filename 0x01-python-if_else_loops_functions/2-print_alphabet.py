da9eee3244ec8e1f07ca2ab7a8e3a7bc0b7831b3
mohamed-622 <mohamedezghoudi@gmail.com>
2023-06-06 11:30:13 -0700

    pycodestyle

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,2 +1,3 @@
 #!/usr/bin/python3
-print(''.join(['{}'.format(chr(i)) for i in range(ord('a'), ord('z') + 1)]), end='')
+print(''.join(['{}'.format(chr(i)) for i in range(ord('a'), ord('z') + 1)]),
+      end='')
