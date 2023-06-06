2f4dc31ec17aa5abb43392ebaade8434a6bfbba0
Mohamed <mohamedezghoudi@gmail.com>
2023-06-06 19:12:42 +0100

    checking the sign before the last digit

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -2,12 +2,16 @@
 import random
 number = random.randint(-10000, 10000)
 
-lastDigit = abs(number) % 10
+if number < 0:
+    lastDigit = -((-1*number) % 10)
+else:
+    lastDigit = number % 10
 
-# check the last digit of the number
 if lastDigit == 0:
-    print(f"Last digit of {number} is {lastDigit:d} and is 0")
+    print(f"Last digit of {number:d} is {lastDigit:d} and is 0")
 elif lastDigit > 5:
-    print(f"Last digit of {number} is {lastDigit:d} and is greater than 5")
+    print(f"Last digit of {number:d} is {lastDigit:d} and is greater\
+ than 5")
 elif lastDigit < 6 and lastDigit != 0:
-    print(f"Last digit of {number} is {lastDigit:d} and is less than 6 and not 0")
+    print(f"Last digit of {number:d} is {lastDigit:d} and is\
+ less than 6 and not 0")
