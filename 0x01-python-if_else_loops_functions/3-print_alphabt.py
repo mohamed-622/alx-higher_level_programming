0a5dae6896f6808961b0d7f6e98cddcac6d4442c
mohamed-622 <mohamedezghoudi@gmail.com>
2023-06-06 11:39:37 -0700

    with .format

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
-print(''.join(chr(i) for i in range(ord('a'), ord('z')+1) if chr(i) not in 'qe'),
+print(''.join(['{}'.format(chr(i)) for i in range(ord('a'), ord('z') + 1) if chr(i) not in 'qe']),
       end='')
