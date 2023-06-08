1d29f6ed9c04da198bd6a806c12898844406767a
mohamed-622 <mohamedezghoudi@gmail.com>
2023-06-08 09:57:12 -0700

    fixing 1 argument not showing

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,14 +1,13 @@
-#!/usr/bin/pytho3
+#!/usr/bin/python3
 from sys import argv
 
 if __name__ == "__main__":
-    num_args = len(argv)
-
+    num_args = len(argv) - 1
     if num_args == 0:
         print("0 arguments.")
     elif num_args == 1:
         print("1 argument:")
     else:
         print("{} arguments:".format(num_args))
-        for i in range(1, len(argv)):
-            print("{}: {}".format(i, argv[i]))
+for i in range(1, len(argv)):
+print("{}: {}".format(i, repr(argv[i])))
