c96a43d9ba5e4523c6e8baf9f10ef2fea6bfd653
mohamed-622 <mohamedezghoudi@gmail.com>
2023-06-08 18:11:46 +0100

    initial code

---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,12 @@
+from sys import argv
+
+if __name__ == "__main__":
+  args = sys.argv[1:]
+  
+  #cast arguments into integers
+  int_args = [int(arg) for arg in args]
+  
+  #calculate the result of the addition
+  result = sum (int_args)
+  
+  print(result)
