bb28eac64a4020f00b4415c46a3a72fce51249b9
Mohamed <mohamedezghoudi@gmail.com>
2023-06-10 16:57:01 +0100

    adding if

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 
 
-# print_list_integer - prints the ints in a list.
-def print_list_integer(my_list=[]):
-    for i in my_list:
-        print("{}".format(i))
+def print_reversed_list_integer(my_list=[]):
+    if my_list:
+        for i in reversed(my_list):
+            print("{:d}".format(i))
+            
