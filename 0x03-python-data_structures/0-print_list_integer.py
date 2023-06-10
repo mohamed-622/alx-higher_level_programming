77b683c162ca8f546bf88a1eb26d4211297c36b8
Mohamed <mohamedezghoudi@gmail.com>
2023-06-10 15:23:55 +0100

    finale

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 
+
+# print_list_integer - prints the ints in a list.
 def print_list_integer(my_list=[]):
-	
-	for num in my_list:
-		print("{}".format(num))
+    for num in my_list:
+        print("{}".format(num))
