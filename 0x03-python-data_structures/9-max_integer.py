3cdab9af1cbf7b7b418a00e0229ca7116e32b96a
mohamed-622 <mohamedezghoudi@gmail.com>
2023-06-11 05:46:23 -0700

    8-9-11

---
+++ 0x03-python-data_structures/9-max_integer.py
@@ -0,0 +1,9 @@
+def max_integer(my_list=[]):
+    if my_list:
+        max_val = my_list[0]
+        for num in my_list:
+            if num > max_val:
+                max_val = num
+        return max_val
+    else:
+        return None
