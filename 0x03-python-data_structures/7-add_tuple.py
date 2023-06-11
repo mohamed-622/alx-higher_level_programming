d9cf73f0e89c3abbbac6525b73055fcee4abcb81
Mohamed <mohamedezghoudi@gmail.com>
2023-06-11 13:49:25 +0100

    not mine

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,17 +1,20 @@
 #!/usr/bin/python3
 
+
 def add_tuple(tuple_a=(), tuple_b=()):
-    # Truncate the tuples to include only the first 2 integers
-    tuple_a = tuple_a[:2]
-    tuple_b = tuple_b[:2]
+    lenA = len(tuple_a)
+    lenB = len(tuple_b)
+# check for tuple_a
+    if lenA < 1:
+        tuple_a = (0, 0)
+    elif lenA < 2:
+        tuple_a = (tuple_a[0], 0)
 
-    # Pad the tuples with zeros if they are smaller than 2 elements
-    tuple_a = tuple_a + (0, 0) if len(tuple_a) < 2 else tuple_a
-    tuple_b = tuple_b + (0, 0) if len(tuple_b) < 2 else tuple_b
+# check for tuple_b
+    if lenB < 1:
+        tuple_b = (0, 0)
+    elif lenB < 2:
+        tuple_b = (tuple_b[0], 0)
 
-    # Compute the addition of the elements
-    sum_1 = tuple_a[0] + tuple_b[0]
-    sum_2 = tuple_a[1] + tuple_b[1]
-
-    # Return the resulting tuple
-    return sum_1, sum_2
+    result = (tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1])
+    return result
