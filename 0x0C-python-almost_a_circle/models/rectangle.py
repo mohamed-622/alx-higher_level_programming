cbc007749603d4cc561b128f078d9d2150865f1f
mohamed-622 <mohamedezghoudi@gmail.com>
2023-07-16 11:56:02 +0100

    indentation solved

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -72,6 +72,7 @@ class Rectangle(Base):
         """
         for _ in range(self.y):
             print()
-            
+        
         for _ in range(self.height):
-            print(" " * self.x + "#" * self.width)
+            print(" " * self.x, end="")
+            print("#" * self.width)
