22297472aaf9217a3a757dcc0dfafa387bd2340b
Mohamed <mohamedezghoudi@gmail.com>
2023-07-18 18:57:37 +0100

    bare exception

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -4,10 +4,14 @@
 def safe_print_list(my_list=[], x=0):
     count = 0
     try:
-        for i in my_list[:x]:
-            print("{}".format(i), end="")
-            count += 1
-    except:
+        count = 0
+        for index in range(x):
+            try:
+                print(my_list[index], end='')
+                count += 1
+            except IndexError:
+                break
+    except Exception:
         pass
     print()
     return count
