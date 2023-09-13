e0461bb8cd2abadc373be12c1f828b18b834c4a2
Mohamed <mohamedezghoudi@gmail.com>
2023-09-13 23:38:09 +0100

    My number

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,1 +1,14 @@
 #!/usr/bin/node
+
+const args = process.argv;
+
+if (args.length < 3) {
+  console.log('Not a number');
+} else {
+  const intValue = parseInt(args[2]);
+  if (!isNaN(intValue)) {
+    console.log('My number:', intValue);
+  } else {
+    console.log('Not a number');
+  }
+}
