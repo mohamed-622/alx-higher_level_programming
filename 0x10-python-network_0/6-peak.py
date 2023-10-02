1cd7ae299f9a1191b8d528d7882d87231b861938
Mohamed <mohamedezghoudi@gmail.com>
2023-10-02 21:57:00 +0100

    more efficient way

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -5,21 +5,16 @@ def find_peak(list_of_integers):
     """Finds a peak in a list of unsorted integers."""
     if list_of_integers is None or list_of_integers == []:
         return None
-    for i in range(len(list_of_integers)):
-        if i == 0:
-            if list_of_integers[i] >= list_of_integers[i + 1]:
-                return list_of_integers[i]
-        elif i == len(list_of_integers) - 1:
-            if list_of_integers[i] >= list_of_integers[i - 1]:
-                return list_of_integers[i]
-        elif (
-            list_of_integers[i] >= list_of_integers[i - 1]
-            and list_of_integers[i] >= list_of_integers[i + 1]
-        ):
-            return list_of_integers[i]
-        elif (
-            list_of_integers[i] <= list_of_integers[i - 1]
-            and list_of_integers[i] >= list_of_integers[i + 1]
-        ):
-            return list_of_integers[i]
-    return None
+    
+    left, right = 0, len(list_of_integers) - 1
+    
+    while left < right:
+        mid = (left + right) // 2
+        
+        # Compare the middle element with its neighbors
+        if list_of_integers[mid] > list_of_integers[mid + 1]:
+            right = mid
+        else:
+            left = mid + 1
+    
+    return list_of_integers[left]
