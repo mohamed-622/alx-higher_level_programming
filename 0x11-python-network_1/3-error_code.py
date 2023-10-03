710de5f5568f98d5be18d48a6cc8ef15bfc34cfd
mohamed-622 <mohamedezgoudi@gmail.com>
2023-10-03 13:33:39 +0100

    task-3 again

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -1,12 +1,11 @@
 #!/usr/bin/python3
-"""Python script that displays the body of the response (decoded in utf-8)"""
-import urllib
+""" Python script that displays the body of the response (decoded in utf-8)"""
+import urllib.request
 import sys
 
 if __name__ == "__main__":
-    url = sys.argv[1]
     try:
-        with urllib.request.urlopen(url) as response:
-            print(response.read().decode('utf-8'))
+        with urllib.request.urlopen(sys.argv[1]) as res:
+            print(res.read().decode("utf-8"))
     except urllib.error.HTTPError as error:
-        print("Error code: {}".format(error.code))
+        print(f"Error code: {error.code}")
