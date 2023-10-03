2235dbe661523cc1762af782042cd4d1f0e6e442
mohamed-622 <mohamedezgoudi@gmail.com>
2023-10-03 15:37:21 +0100

    github api

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -1,1 +1,11 @@
 #!/usr/bin/python3
+""" Python script that returnes the id of a github user"""
+import requests
+import sys
+
+if __name__ == "__main__":
+    url = "https://api.github.com/user"
+    user = sys.argv[1]
+    password = sys.argv[2]
+    res = requests.get(url, auth=(user, password))
+    print(res.json().get("id"))
