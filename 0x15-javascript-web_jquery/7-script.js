78181ed4300f8b99fd7a15d0b3d8ef44238fee33
mohamed-622 <mohamedezgoudi@gmail.com>
2023-11-02 01:54:29 +0100

    5-9

---
+++ 0x15-javascript-web_jquery/7-script.js
@@ -0,0 +1,7 @@
+$.ajax({
+  url: 'https://swapi-api.hbtn.io/api/people/5/?format=json',
+  type: 'GET',
+  success: function (data) {
+    $('#character').text(data.name);
+  }
+});
---
+++ 0x15-javascript-web_jquery/8-script.js
@@ -0,0 +1,9 @@
+$.ajax({
+  url: 'https://swapi-api.alx-tools.com/api/films/?format=json',
+  type: 'GET',
+  success: function (data) {
+    for (const movie of data.results) {
+      $('UL#list_movies').append('<li>' + movie.title + '</li>');
+    }
+  }
+});
